df5f2f07931f8ae559a88165eeb76a9692b917bc
Chris Anatalio <chris.anatalio@opengov.com>
2018-10-05 02:11:04 -0700

    Initial commit

---
+++ src/main/java/com/linkedin/learning/reactivespring/ReactivespringApplication.java
@@ -0,0 +1,12 @@
+package com.linkedin.learning.reactivespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivespringApplication.class, args);
+	}
+}
