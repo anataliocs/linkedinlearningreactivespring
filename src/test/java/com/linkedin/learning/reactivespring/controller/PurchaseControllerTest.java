33ff02d91b8230ad71e8826f80cbf9b12512a2d6
Chris Anatalio <chris.anatalio@opengov.com>
2018-10-13 20:23:47 -0700

    Add in test

--- src/test/java/com/linkedin/learning/reactivespring/controller/PurchaseControllerTest.java
+++ src/test/java/com/linkedin/learning/reactivespring/controller/PurchaseControllerTest.java
@@ -1,38 +1,33 @@
 package com.linkedin.learning.reactivespring.controller;
 
-import static org.junit.Assert.*;
-
 import com.linkedin.learning.reactivespring.model.Purchase;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@WebFluxTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PurchaseControllerTest {
 
-  private WebTestClient client;
-
-
+  @Autowired
+  ApplicationContext context;
+  private WebTestClient webTestClient;
 
   @Before
   public void setUp() throws Exception {
-    client = WebTestClient
-        .bindToController(new PurchaseController())
+    webTestClient = WebTestClient
+        .bindToApplicationContext(this.context)
         .build();
-  }
-
-  @After
-  public void tearDown() throws Exception {
   }
 
   @Test
   public void createPurchase() throws Exception {
-    client.get()
+    webTestClient.get()
         .uri("/coin/purchase/v1/{id}", 123)
         .exchange()
         .expectStatus().isOk()
--- src/test/java/com/linkedin/learning/reactivespring/service/CoinbaseServiceImplTest.java
+++ src/test/java/com/linkedin/learning/reactivespring/service/CoinbaseServiceImplTest.java
@@ -1,16 +1,48 @@
 package com.linkedin.learning.reactivespring.service;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.linkedin.learning.reactivespring.config.ApiConfig;
+import com.linkedin.learning.reactivespring.config.DataConfig;
+import com.linkedin.learning.reactivespring.model.CoinBaseResponse;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes={ApiConfig.class, DataConfig.class})
+@ComponentScan
+@EnableAutoConfiguration
 public class CoinbaseServiceImplTest {
 
+  @Autowired
+  private ObjectWriter objectWriter;
+
+  @Autowired
+  private WebClient webClient;
+
+  @Autowired
+  private ReactiveMongoOperations reactiveMongoTemplate;
+
+  @Autowired
   CoinbaseService classUnderTest;
 
   @Before
   public void setUp() throws Exception {
-    classUnderTest = new CoinbaseServiceImpl();
+
   }
 
   @After
@@ -19,14 +51,8 @@ public class CoinbaseServiceImplTest {
 
   @Test
   public void getCryptoPrice() throws Exception {
-    classUnderTest.getPurchaseById("id");
-  }
+    Mono<CoinBaseResponse> responseMono = classUnderTest.getCryptoPrice("BTC-USD");
 
-  @Test
-  public void createPurchase() throws Exception {
-  }
-
-  @Test
-  public void getPurchase() throws Exception {
+    responseMono.subscribe(System.out::println);
   }
 }
